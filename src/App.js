b284e951f832789d072a4151c51157a94182389c
Ugljesa Pantic <ugljesapantic@gmail.com>
2019-02-05 10:46:44 +0100

    added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+const AppWrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(0deg, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0.6) 100%);
+`
+
+const AppTitle = styled.h1`
+  margin-bottom: 3rem;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppWrapper className="App">
+        <AppTitle>Welcome to Personal Issue tracking System</AppTitle>
+        <Button primary size='massive'>SIGN UP</Button>
+      </AppWrapper>
     );
   }
 }
