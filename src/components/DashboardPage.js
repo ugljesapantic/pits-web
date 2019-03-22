d460163ad8829881d0c4220fced1cb56ad6bb047
Ugljesa Pantic <ugljesapantic@gmail.com>
2019-03-22 16:52:27 +0100

    rr

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Greeting = styled.div`
+  font-size: 3rem;
+  line-height: 5rem;
+  text-align: center;  
+  transform: translateY(calc(50vh - 10rem));
+`;
 
 class DashboardPage extends Component {
 
     render() {
         
         return (
-            <div>Welcome to the dashboard</div>
+            <Greeting>Welcome to the PITS</Greeting>
         );
     }
 }
